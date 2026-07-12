9af9ffa3a7f575cf0ee62944e4afd23b597c3cf3
Filippo Giacchè <filippo.giacche@gmail.com>
2026-07-12 14:52:09 +0200

    feat: wire application theme and global styles

---
+++ src/main.tsx
@@ -0,0 +1,15 @@
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { ThemeProvider } from '@emotion/react';
+import App from './App';
+import { GlobalStyles } from './globalStyles';
+import { theme } from './theme';
+
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <ThemeProvider theme={theme}>
+      <GlobalStyles theme={theme} />
+      <App />
+    </ThemeProvider>
+  </StrictMode>,
+);
